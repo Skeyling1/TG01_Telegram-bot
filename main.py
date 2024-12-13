8454508467a91fbcc1129f01bf06b03c75de7693
Skeyling1 <skeyling1@gmail.com>
2024-12-13 10:57:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-
 from config import TOKEN
-
+import random
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+
+@dp.message(Command('photo'))
+async def photo(message: Message):
+    list = ['https://img.freepik.com/free-vector/hand-drawn-valentine-s-day-cats-couple_23-2148398963.jpg?semt=ais_hybrid', 'https://img.freepik.com/free-vector/flat-cute-animal-cat-space-illustration-kids-cute-cat-character_69135-1249.jpg?semt=ais_hybrid', 'https://img.freepik.com/free-vector/cute-valentine-s-day-animal-couple_23-2148390534.jpg?semt=ais_hybrid']
+    rand_photo = random.choice(list)
+    await message.answer_photo(photo=rand_photo, caption='это супер крутая картинка')
+
+@dp.message(F.photo)
+async def react_photo(message: Message):
+    list = ['Ого какая фотка!', 'непонятно что это', 'не отправляй такое больше!']
+    rand_answ = random.choice(list)
+    await message.answer(rand_answ)
+
+@dp.message(F.text == "Что такое ИИ?")
+async def ai_text(message: Message):
+    await message.answer('Иску́сственный интелле́кт (англ. artificial intelligence; AI) в самом широком смысле — это интеллект, демонстрируемый машинами, в частности компьютерными системами.')
+
+
+@dp.message(Command('help'))
+async def help(message: Message):
+    await message.answer('Этот бот умеет выполнять команды: \n /start \n /help \n /photo \n /weather')
 
 @dp.message(CommandStart)
 async def start(message: Message):
@@ -14,7 +35,7 @@ async def start(message: Message):
 
 
 async def main():
-    await dp.stop_polling(bot)
+    await dp.start_polling(bot)
 
 
 if __name__ =='__main__':
