b77d4b3c7309b29b03746e20e9ecab42ebbe6688
Skeyling1 <skeyling1@gmail.com>
2024-12-17 19:04:09 +0300

    Update new.py

--- new.py
+++ new.py
@@ -96,6 +96,5 @@ async def city(message: Message, state: FSMContext):
 async def main():
     await dp.start_polling(bot)
 
-
-if __name__ =='__main__':
-    asyncio.run(main())
+asyncio.run(main())
+# if __name__ =='__main__':
