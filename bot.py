548becba325613a677d5f0000960c6fb85d91e6c
Skeyling1 <skeyling1@gmail.com>
2024-12-24 16:42:33 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,92 @@
+import asyncio
+import sqlite3
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from config import TOKEN
+import random
+import requests
+from gtts import gTTS
+import os
+import keyboards as kb
+
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+button_registr = KeyboardButton(text="Регистрация в телеграм боте")
+button_exchange_rates = KeyboardButton(text="Курс валют")
+button_tips = KeyboardButton(text="Советы по экономии")
+button_finances = KeyboardButton(text="Личные финансы")
+
+
+keyboards = ReplyKeyboardMarkup(keyboard=[
+    [button_registr, button_exchange_rates],
+    [button_tips, button_finances],
+    ], resize_keyboard=True)
+
+conn = sqlite3.connect('user.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY,
+    telegram_id INTEGER UNIQUE,
+    name TEXT,
+    category1 TEXT,
+    category2 TEXT,
+    category3 TEXT,
+    expenses1 REAL,
+    expenses1 REAL,
+    expenses1 REAL
+    )
+    ''')
+
+conn.commit()
+
+class FinancesForm(StatesGroup):
+    category1 = State()
+    expenses1 = State()
+    category2 = State()
+    expenses2 = State()
+    category3 = State()
+    expenses3 = State()
+
+
+@dp.message(CommandStart())
+async def send_start(message: Message):
+    await message.answer("Пирвет! Я ваш личный финансовый помошник. Выберете одну из функций.", reply_markup=keyboards)
+
+
+@dp.message(F.text == "Регистрация в телеграм боте")
+async def registration(message: Message):
+    telegram_id = message.from_user.id
+    name = message.from_user.full_name
+    cursor.execute('''SELECT * FROM users WHERE telegram_id = ?''', (telegram_id,))
+    user = cursor.fetchone()
+    if user:
+        await message.answer("Вы уже зарегистрированы.")
+    else:
+        cursor.execute('''INSERT INTO users (telegram_id, name) VALUES (?, ?)''', (telegram_id, name))
+        conn.commit()
+        await message.answer("Вы успешно зарегистрированы!")
+
+
+@dp.message(F.text == "Курс валют")
+async def exchange_rates(message: Message):
+    url = "https://v6.exchangerate-api.com/v6/6c8bd6bf65e9398c96399d5f/latest/USD"
+    
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ =='__main__':
+    asyncio.run(main())
